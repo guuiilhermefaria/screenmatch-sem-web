40259a144bd9b6d045a555d4db8fc3963cf77dad
Guilherme Faria <guuiilhermefaria@gmail.com>
2024-10-15 16:13:59 -0300

    Last Updates from Screenmatch project.

--- src/main/java/br/com/guilherme/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/guilherme/screenmatch/controller/SerieController.java
@@ -1,23 +1,60 @@
 package br.com.guilherme.screenmatch.controller;
 
+import br.com.guilherme.screenmatch.dto.EpisodioDTO;
 import br.com.guilherme.screenmatch.dto.SerieDTO;
-import br.com.guilherme.screenmatch.repository.SerieRepository;
+import br.com.guilherme.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
-    @Autowired
-    private SerieRepository repository;
 
-    @GetMapping("/series")
+    @Autowired
+    private SerieService servico;
+
+    @GetMapping
     public List<SerieDTO> obterSeries() {
-        return repository.findAll().stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+        return servico.obterTodasAsSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series() {
+        return servico.obterTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> obterLancamento() {
+        return servico.obterLancamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterSeriePorId(@PathVariable Long id) {
+        return servico.obterSeriePorId(id);
+    }
+
+    @GetMapping("/{id}/temporadas/todas")
+    public List<EpisodioDTO> obterTodasTemporadas(@PathVariable Long id){
+        return servico.obterTodasTemporadas(id);
+    }
+
+    @GetMapping("/{id}/temporadas/{numero}")
+    public List<EpisodioDTO> obterTemporadasPorNumero(@PathVariable Long id, @PathVariable Long numero){
+        return servico.obterTemporadasPorNumero(id, numero);
+    }
+
+    @GetMapping("/categoria/{nomeGenero}")
+    public List<SerieDTO> obterSeriesPorCategoria(@PathVariable String nomeGenero){
+        return servico.obterSeriesPorCategoria(nomeGenero);
+    }
+
+    @GetMapping("/{id}/temporadas/top")
+    public List<EpisodioDTO> obterTop5Episodios(@PathVariable Long id){
+        return servico.obterTop5Episodios(id);
     }
  }
---
+++ src/main/java/br/com/guilherme/screenmatch/dto/EpisodioDTO.java
@@ -0,0 +1,6 @@
+package br.com.guilherme.screenmatch.dto;
+
+public record EpisodioDTO(Integer temporada,
+                          Integer numeroEpisodio,
+                          String titulo) {
+}
